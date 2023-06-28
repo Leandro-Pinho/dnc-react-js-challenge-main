e1e12a1a3cbea2a530acd30f3e2a54e8bf33b3f7
Leandro-Pinho <leandroessencia@gmail.com>
2023-06-27 21:59:40 -0300

    first commit - adicionando o header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './index.scss'
+
+const Header = () => {
+    return (
+        <header>
+            <ul>
+                <li>Organização</li>
+                <li className='tarefa'>Tarefas</li>
+            </ul>
+        </header>
+    )
+}
+
+export default Header
